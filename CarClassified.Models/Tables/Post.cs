4fef4dfdb546e0dcffd11b6855098fe41a826109
Derick Bonsu <dbonsu@catalystdevworks.com>
2016-04-18 12:03:51 -0400

    boilder plate models-

---
+++ CarClassified.Models/Tables/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassified.Models.Tables
+{
+    public class Post
+    {
+        //change spelling of title in db
+        public int Id { get; set; }
+
+        public int Title { get; set; }
+        public string Body { get; set; }
+        public bool IsActive { get; set; }
+        public string Location { get; set; }
+        public Guid PosterId { get; set; }
+        public int StateId { get; set; }
+        public DateTime PostDate { get; set; }
+    }
+}
