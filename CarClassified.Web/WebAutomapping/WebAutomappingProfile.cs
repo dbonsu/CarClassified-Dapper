3c45a55a95daedd363b9fd9c4c34b8b623373577
Derick Bonsu <dbonsu@catalystdevworks.com>
2016-05-02 10:03:17 -0400

    edits

--- CarClassified.Web/WebAutomapping/WebAutomappingProfile.cs
+++ CarClassified.Web/WebAutomapping/WebAutomappingProfile.cs
@@ -2,10 +2,7 @@
 using CarClassified.Models.Tables;
 using CarClassified.Models.Views;
 using CarClassified.Web.ViewModels;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace CarClassified.Web.WebAutomapping
 {
